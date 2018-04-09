d3898693512a153ba17aced6cf25203d2d3dadbb
Lior Rabin <lior@colu.com>
2018-04-09 10:46:19 +0300

    https://github.com/colucom/osseus-server/issues/1

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const path = require('path')
 
 const init = function (config) {
-  const self = this
   return new Promise((resolve, reject) => {
     config.get = function (key) {
       return config.server[key.toLowerCase()] || config[key.toLowerCase()]
     }
     const app = require(path.join(__dirname, '/lib/app'))(config)
-    self.app = app
+    this.app = app
     resolve(app)
   })
 }
 
 const start = async function (config) {
-  const self = this
   const port = config.server.port || config.port
-  const server = await self.app.listen(port)
+  const server = await this.app.listen(port)
   return server
 }
 
