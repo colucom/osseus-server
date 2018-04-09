90687c65d3288e06148111e51530ddf7e7bd72c5
Tal Beja <tal@colu.com>
2018-04-09 17:11:22 +0300

    get osseus to init config from osseus no param to start app and server added to this and not to as return value

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const path = require('path')
 
-const init = function (config) {
+const init = function (osseus) {
+  this.osseus = osseus
+  const config = osseus.config
   return new Promise((resolve, reject) => {
     config.get = function (key) {
       return config.osseus_server[key.toLowerCase()] || config[key.toLowerCase()]
     }
     const app = require(path.join(__dirname, '/lib/app'))(config)
     this.app = app
-    resolve(app)
+    resolve(this)
   })
 }
 
-const start = async function (config) {
+const start = async function () {
+  const config = this.osseus.config
   const port = config.osseus_server.port || config.port
   const server = await this.app.listen(port)
-  return server
+  this.server = server
 }
 
 module.exports = {
