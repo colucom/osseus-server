af72f4f9e424d94646a031b2fa2726800a2bd85a
Lior Rabin <lior@colu.com>
2018-04-10 14:05:38 +0300

    remove config.get

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ const path = require('path')
 
 const init = function (osseus) {
   this.osseus = osseus
-  const config = osseus.config
   return new Promise((resolve, reject) => {
-    config.get = function (key) {
-      return config.osseus_server[key.toLowerCase()] || config[key.toLowerCase()]
-    }
-    const app = require(path.join(__dirname, '/lib/app'))(config)
+    const app = require(path.join(__dirname, '/lib/app'))(osseus.config)
     this.app = app
     resolve(this)
   })
 }
 
 const start = async function () {
-  const config = this.osseus.config
-  const port = config.osseus_server.port || config.port
+  const port = this.osseus.config.osseus_server.port || this.osseus.config.port
   const server = await this.app.listen(port)
   console.info(`server is listening on port: ${server.address().port}`)
   this.server = server
