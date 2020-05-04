3de31cb02c2540ab65f729bbe0a746ab1f5589b9
Roman Plotkin <roman@colu.com>
2020-05-05 01:03:19 +0300

    errors handler middlewares added.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const path = require("path")
+const path = require('path')
+
+const { MoleculerError } = require('moleculer').Errors
 
 const init = function (osseus) {
   this.osseus = osseus
   return new Promise(async (resolve, reject) => {
-    const app = require(path.join(__dirname, "/lib/app"))(osseus.config)
+    const app = require(path.join(__dirname, '/lib/app'))(osseus.config)
     this.app = app
 
     // Load the osseus-moleculer-web module if needed
@@ -12,9 +14,9 @@ const init = function (osseus) {
       false
     if (shouldUseMoleculerWebAsMiddleware) {
       try {
-        const OsseusMoleculerWeb = require("@colucom/osseus-moleculerweb")
+        const OsseusMoleculerWeb = require('@colucom/osseus-moleculerweb')
         const moleculerObj = await OsseusMoleculerWeb.init(osseus)
-        osseus["moleculer"] = moleculerObj.moleculer.broker
+        osseus['moleculer'] = moleculerObj.moleculer.broker
 
         this.moleculerWebService = moleculerObj.moleculer.service
       } catch (err) {
@@ -28,9 +30,7 @@ const init = function (osseus) {
 
 const start = function () {
   return new Promise((resolve, reject) => {
-
     // Load middelwares and add them to the express
-
     const cwd = process.cwd()
     const envMiddewaresValue = this.osseus.config.osseus_server.middlewares_path
     if (envMiddewaresValue) {
@@ -45,12 +45,106 @@ const start = function () {
 
     //Adding moleculer-web api gateway service as middleware to express if needed
     if (this.moleculerWebService) {
-      this.app.use("/api", this.moleculerWebService.express())
+      this.app.use('/api', this.moleculerWebService.express())
     }
 
-
     // Configure error middlewares
-    // app.use((err, req, res, next))...
+    let errorsMap
+    const errorsMapConfigValue = this.osseus.config.osseus_server
+      .errors_map_path
+    if (errorsMapConfigValue) {
+      try {
+        const errorsMapPath = path.join(cwd, errorsMapConfigValue)
+        errorsMap = require(errorsMapPath)
+      } catch (err) {
+        errorsMap = null
+        console.log('Failed to load errors map file.')
+      }
+    }
+
+    this.app.use((err, req, res, next) => {
+      if (!err) {
+        next()
+      }
+
+      try {
+        this.app.logger.debug(`Error | ${JSON.stringify(err)}`)
+      } catch (ex) {
+        try {
+          this.app.logger.debug(`Error Data | ${JSON.stringify(err.data)}`)
+        } catch (e) {
+          this.app.logger.debug(`Error(no stringify) | ${err}`)
+        }
+      }
+
+      try {
+        if (typeof err === 'string') {
+          err = JSON.parse(err)
+        }
+      } catch (ex) {
+        console.log('Failed to parse json')
+      }
+
+      if (err instanceof MoleculerError) {
+        const name = (err.data && err.data.name) || 'MoleculerError'
+        const code =
+          (err.data && err.data.code) ||
+          (errorsMap && errorsMap[err.code] && errorsMap[err.code].code) ||
+          999999500
+        console.warn(`Moleculer error occured. Name: ${name}, Code: ${code}`)
+
+        next({
+          message:
+            (err.data && err.data.message) ||
+            err.message ||
+            'Moleculer error occured',
+          name,
+          status:
+            (err.data && err.data.status) ||
+            (errorsMap[err.code] && errorsMap[err.code].status) ||
+            500,
+          code,
+        })
+      } else {
+        next(err.error || err)
+      }
+    })
+
+    this.app.use((err, req, res, next) => {
+      if (err && err.status) {
+        let display = {
+          message: err.message,
+          name: err.name,
+          status: err.status,
+          code: err.code,
+        }
+
+        let response
+
+        const smsCode =
+          this.osseus.config.osseus_server.sms_err_code_prefix || 611
+        if (err.code && err.code.toString().startsWith(smsCode)) {
+          const smsError = {
+            message: display.message,
+            status: display.status,
+          }
+          response = { error: display, ...smsError }
+        } else {
+          response = { error: display }
+        }
+
+        console.error(
+          'end of stach with status',
+          JSON.stringify({ ...response, data: err.data })
+        )
+
+        res.status(err.status).send(response)
+      } else {
+        delete err.ctx
+        console.error('end of stack error', err)
+        res.status(500).send({ error: err })
+      }
+    })
 
     const port =
       this.osseus.config.osseus_server.port || this.osseus.config.port
@@ -60,7 +154,7 @@ const start = function () {
         this.server = server
         resolve()
       })
-      .on("error", (err) => {
+      .on('error', (err) => {
         reject(err)
       })
   })
